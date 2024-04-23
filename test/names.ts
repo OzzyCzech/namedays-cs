bea6faca8dc47a551b4f65e0d3e27f6d4a31bbc1
Roman Ožana <roman@ozana.cz>
2024-04-23 09:35:34 +0200

    Fix tests

--- test/names.ts
+++ test/names.ts
@@ -34,24 +34,39 @@ test('Check Horymír at leap year', t => {
 	t.is(getNameDay(froIso('2024-02-29')), 'Horymír');
 });
 
+test('Check Tobiáš at All Souls Day', t => {
+	t.deepEqual(getNameDayArray(froIso('2024-11-02')), ['Tobiáš']);
+	t.is(getNameDay(froIso('2024-11-02')), 'Tobiáš');
+});
+
+test('Check Jidáš and Alfréd at Independence Day', t => {
+	t.deepEqual(getNameDayArray(froIso('2024-10-28')), ['Jidáš', 'Alfréd']);
+	t.is(getNameDay(froIso('2024-10-28')), 'Jidáš a Alfréd');
+});
+
+test('St. Cyril and Methodius Day', t => {
+	t.deepEqual(getNameDayArray(froIso('2024-07-05')), ['Cyril', 'Metoděj']);
+	t.is(getNameDay(froIso('2024-07-05')), 'Cyril a Metoděj');
+});
+
 test('Check days without names', t => {
 	// New Year's Day
 	t.deepEqual(getNameDayArray(froIso('2024-01-01')), []);
 	t.is(getNameDay(froIso('2024-01-01')), '');
 
-	// All Souls Day
-	t.deepEqual(getNameDayArray(froIso('2024-11-02')), []);
-	t.is(getNameDay(froIso('2024-11-02')), '');
+	// Labour Day
+	t.deepEqual(getNameDayArray(froIso('2024-05-01')), []);
+	t.is(getNameDay(froIso('2024-05-01')), '');
+
+	// End of World War II
+	t.deepEqual(getNameDayArray(froIso('2024-05-08')), []);
+	t.is(getNameDay(froIso('2024-05-08')), '');
+
+	// Three Kings' Day
+	t.deepEqual(getNameDayArray(froIso('2024-01-06')), []);
+	t.is(getNameDay(froIso('2024-01-06')), '');
 
 	// Christmas Day
 	t.deepEqual(getNameDayArray(froIso('2024-12-25')), []);
 	t.is(getNameDay(froIso('2024-12-25')), '');
-
-	// Independence Day
-	t.deepEqual(getNameDayArray(froIso('2024-10-28')), []);
-	t.is(getNameDay(froIso('2024-10-28')), '');
-
-	// St. Cyril and Methodius Day
-	t.deepEqual(getNameDayArray(froIso('2024-07-05')), ['Cyril', 'Metoděj']);
-	t.is(getNameDay(froIso('2024-07-05')), 'Cyril a Metoděj');
 });
