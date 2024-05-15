719924b01fff23aa9317bbd5e9fbb28f8d01a910
Roman Ožana <roman@ozana.cz>
2024-05-15 09:59:03 +0200

    Remove concat function (it's easy concat string in code)

--- lib/names.ts
+++ lib/names.ts
@@ -5,27 +5,8 @@ const names = data as Record<string, string[]>;
 /**
  * Get names as a string for the given date
  * @param date
- * @param joinWith
  */
-export function getNameDay(date: Date, joinWith = ' a '): string {
-	return getNameDayArray(date)?.join(joinWith) ?? '';
-}
-
-/**
- * Get the key for the given date
- * Key is a string in the format ddMM
- * @param date
- */
-function getKey(date: Date): string {
-	return date.getDate().toString().padStart(2, '0')
-		+ (date.getMonth() + 1).toString().padStart(2, '0');
-}
-
-/**
- * Get the names of the name day for the given date
- * @param date
- */
-export function getNameDayArray(date: Date): string[] | undefined {
-	const day = getKey(date);
-	return day in names ? names[day] : [];
+export function getNameDay(date: Date): string[] {
+	const key = `${(date.getMonth() + 1).toString().padStart(2, '0')}-${(date.getDate()).toString().padStart(2, '0')}`;
+	return names[key] ?? [];
 }
--- test/names.ts
+++ test/names.ts
@@ -1,11 +1,11 @@
 import test from 'ava';
-import {getNameDayArray, getNameDay} from '../lib/index.js';
+import {getNameDay} from '../lib/index.js';
 
 /**
  * Convert ISO date string to Date object
  * @param date
  */
-function froIso(date: string): Date {
+function fromIso(date: string): Date {
 	const [year, month, day] = date.split('-').map(Number);
 
 	if (year === undefined || month === undefined || day === undefined) {
@@ -15,58 +15,53 @@ function froIso(date: string): Date {
 	return new Date(year, month - 1, day);
 }
 
-test('Check some significant names', t => {
+test('Check Štěpánka name', t => {
+	const date = fromIso('2024-10-31');
+	t.deepEqual(getNameDay(date), ['Štěpánka']);
+});
+
+test('Check Zdeněk name', t => {
 	// 2024-10-31 is the day of Štěpánka
-	t.deepEqual(getNameDayArray(froIso('2024-10-31')), ['Štěpánka']);
-	t.is(getNameDay(froIso('2024-10-31')), 'Štěpánka');
+	const date = fromIso('2024-01-23');
+	t.deepEqual(getNameDay(date), ['Zdeněk']);
+});
 
-	// 2024-01-23 is the day of Zdeněk
-	t.deepEqual(getNameDayArray(froIso('2024-01-23')), ['Zdeněk']);
-	t.is(getNameDay(froIso('2024-01-23')), 'Zdeněk');
-
-	// 2024-12-24 is the day of Adam and Eve (Adam a Eva)
-	t.deepEqual(getNameDayArray(froIso('2024-12-24')), ['Adam', 'Eva']);
-	t.is(getNameDay(froIso('2024-12-24')), 'Adam a Eva');
+test('Check Adam, Eva names', t => {
+	const date = fromIso('2024-12-24');
+	t.deepEqual(getNameDay(date), ['Adam', 'Eva']);
 });
 
 test('Check Horymír at leap year', t => {
-	t.deepEqual(getNameDayArray(froIso('2024-02-29')), ['Horymír']);
-	t.is(getNameDay(froIso('2024-02-29')), 'Horymír');
+	const date = fromIso('2024-02-29');
+	t.deepEqual(getNameDay(date), ['Horymír']);
 });
 
 test('Check Tobiáš at All Souls Day', t => {
-	t.deepEqual(getNameDayArray(froIso('2024-11-02')), ['Tobiáš']);
-	t.is(getNameDay(froIso('2024-11-02')), 'Tobiáš');
+	const date = fromIso('2024-11-02');
+	t.deepEqual(getNameDay(date), ['Tobiáš']);
 });
 
 test('Check Jidáš and Alfréd at Independence Day', t => {
-	t.deepEqual(getNameDayArray(froIso('2024-10-28')), ['Jidáš', 'Alfréd']);
-	t.is(getNameDay(froIso('2024-10-28')), 'Jidáš a Alfréd');
+	t.deepEqual(getNameDay(fromIso('2024-10-28')), ['Jidáš', 'Alfréd']);
 });
 
 test('St. Cyril and Methodius Day', t => {
-	t.deepEqual(getNameDayArray(froIso('2024-07-05')), ['Cyril', 'Metoděj']);
-	t.is(getNameDay(froIso('2024-07-05')), 'Cyril a Metoděj');
+	t.deepEqual(getNameDay(fromIso('2024-07-05')), ['Cyril', 'Metoděj']);
 });
 
 test('Check days without names', t => {
 	// New Year's Day
-	t.deepEqual(getNameDayArray(froIso('2024-01-01')), []);
-	t.is(getNameDay(froIso('2024-01-01')), '');
+	t.deepEqual(getNameDay(fromIso('2024-01-01')), []);
 
 	// Labour Day
-	t.deepEqual(getNameDayArray(froIso('2024-05-01')), []);
-	t.is(getNameDay(froIso('2024-05-01')), '');
+	t.deepEqual(getNameDay(fromIso('2024-05-01')), []);
 
 	// End of World War II
-	t.deepEqual(getNameDayArray(froIso('2024-05-08')), []);
-	t.is(getNameDay(froIso('2024-05-08')), '');
+	t.deepEqual(getNameDay(fromIso('2024-05-08')), []);
 
 	// Three Kings' Day
-	t.deepEqual(getNameDayArray(froIso('2024-01-06')), []);
-	t.is(getNameDay(froIso('2024-01-06')), '');
+	t.deepEqual(getNameDay(fromIso('2024-01-06')), []);
 
 	// Christmas Day
-	t.deepEqual(getNameDayArray(froIso('2024-12-25')), []);
-	t.is(getNameDay(froIso('2024-12-25')), '');
+	t.deepEqual(getNameDay(fromIso('2024-12-25')), []);
 });
